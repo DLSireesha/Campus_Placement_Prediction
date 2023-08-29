f4ea929d63c4afe4cd1edc032320241377aa2109
Sireesha18 <116700405+DLSireesha@users.noreply.github.com>
2023-08-29 15:26:40 +0530

    Update Campus_Placement_Prediction_app.py

--- Campus_Placement_Prediction_app.py
+++ Campus_Placement_Prediction_app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import pickle
 
-model_path='C:/Users/hp/Desktop/python/campus_placement.pkl'
-model = pickle.load(open(model_path, 'rb'))
+model = pickle.load(open('campus_placement.sav','rb'))
 
 
 
